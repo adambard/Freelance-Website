2fc02829ccdf622a4c1ecff8dd0ae22b5388a771
Adam Bard <github@adambard.com>
2011-01-02 10:53:23 -0800

    Adjustments for responsive design (resize it)

--- media/js/script.js
+++ media/js/script.js
@@ -1,8 +1,22 @@
-/* Author: 
+/* Author: Adam Bard
 
 */
 
+function onResize(){
+	var width = $(window).width();
+	b = $('body')
+	b.removeClass('narrow').removeClass('mobile')
 
+	if(width <= 480){
+		b.addClass('mobile')
+	}
+	if(width <= 940){
+		b.addClass('narrow')
+	}
+}
+
+$(window).resize(onResize);
+onResize();
 
 
 
